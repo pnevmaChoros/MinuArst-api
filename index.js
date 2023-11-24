8b3269c7042ea7295b709698629c752024c20e51
daniel dogadajev <daniel.dogadajev@gmail.com>
2023-11-24 15:37:07 +0200

    swagger-express

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const app = require('express')()
 const port = 8080
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./docs/swagger.json');
+
+app.use('/docs', swaggerUi.serve, swaggerUisetup(swaggerDocument))
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)
