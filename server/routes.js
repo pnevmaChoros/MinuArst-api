cd74c5149b79fbf1d882458ee09491485b62c618
daniel dogadajev <daniel.dogadajev@gmail.com>
2024-01-18 16:45:19 +0200

    Get list of all Patient [#186868526]

--- server/routes.js
+++
@@ -1,8 +0,0 @@
-const patientController = require('./controllers/PatientController');
-
-module.exports = (app) => {
-
-    //patient/
-    app.route('/patient')
-    .get(patientController.getAll)
-}
